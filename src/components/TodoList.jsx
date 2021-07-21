fba49d51a65ee0b01aa5b43174857523c62b8657
N. Martin Abregu <nmabregu@gmail.com>
2021-07-21 17:19:22 -0300

    uuid key unique

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { TodoItem } from './TodoItem';
 
 export function TodoList({ todos }) {
     return (
         <ul>
-            { todos.map((todo) => (
-                <li>Tarea</li>
-            )) }
+            { 
+                todos.map((todo) => (
+                    <TodoItem key={todo.id} todo={todo} />
+                )) 
+            }
         </ul>
     )
 }
